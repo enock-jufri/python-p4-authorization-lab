5bfa2691dca139b904dc50751674438a615647fb
enock-jufri <enockjufri@gmail.com>
2025-02-20 21:42:08 +0300

    limiting users to cartain views

--- server/app.py
+++ server/app.py
@@ -18,6 +18,10 @@ db.init_app(app)
 
 api = Api(app)
 
+def check_if_logged_in():
+    if not session.get('user_id') and request.endpoint not in {'member_article', 'member_index'}:
+        return {'error': 'Unauthorized'}, 401
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -87,12 +91,16 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        articles=[article.to_dict() for article in Article.query.filter(Article.is_member_only==True).all()]
+
+        return make_response(articles,200)
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        article = Article.query.filter(Article.id == id).first()
+        return make_response(article,200)
+        
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
